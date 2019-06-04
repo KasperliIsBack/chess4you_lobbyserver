3ef746a624006d8ec467e287382d8fdf3cfbc50d
Andri Pieren <andri.pieren>
2019-06-04 17:58:41 +0200

    add missing files

---
+++ lobbyserver/src/main/java/com/chess4you/lobbyserver/LobbyServerApplication.java
@@ -0,0 +1,13 @@
+package com.chess4you.lobbyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobbyServerApplication.class, args);
+	}
+
+}
---
+++ lobbyserver/src/test/java/com/chess4you/lobbyserver/LobbyServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chess4you.lobbyserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LobbyServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
