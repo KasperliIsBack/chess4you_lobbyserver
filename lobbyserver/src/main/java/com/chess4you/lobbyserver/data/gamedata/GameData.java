65981933f455268cfbb542e38af620e4ea114500
Andri Pieren <andri.pieren>
2019-06-27 17:19:03 +0200

    edit GameData

--- lobbyserver/src/main/java/com/chess4you/lobbyserver/data/gamedata/GameData.java
+++ lobbyserver/src/main/java/com/chess4you/lobbyserver/data/gamedata/GameData.java
@@ -2,13 +2,17 @@ package com.chess4you.lobbyserver.data.gamedata;
 
 import com.chess4you.lobbyserver.data.Player;
 import com.chess4you.lobbyserver.data.enums.Color;
+import com.google.gson.Gson;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.var;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Document(collection = "gameData")
@@ -27,5 +31,19 @@ public class GameData {
     private int gamePeriodInMinute;
     private Date turnDate;
     private Movement[] historyOfMovements;
-    private Map<Position, Piece> mapPosPiece;
+    private String mapPosPiece;
+
+    public void setMapPosPiece(Map<Position, Piece> mapPosPiece) {
+        List<Piece> listPiece = new ArrayList<>();
+        listPiece.addAll(mapPosPiece.values());
+        List<Position> listPosition = new ArrayList<>();
+        listPosition.addAll(mapPosPiece.keySet());
+
+        for (int index = 0; index < listPiece.size(); index++) {
+            var piece = listPiece.get(index);
+            piece.setPosition(listPosition.get(index));
+            listPiece.set(index, piece);
+        }
+        this.mapPosPiece = new Gson().toJson(listPiece);
+    }
 }
